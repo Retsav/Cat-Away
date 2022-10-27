14d1e2da664c13e94191e2c7ce69e41ba1b53f5a
Retsav <ddadanski@edu.cdv.pl>
2022-10-27 18:48:34 +0200

    Kod Spawnera

---
+++ CatAway/Assets/Scripts/V_SpawnerManager.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class V_SpawnerManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<ObjectSpawner> objectSpawners;
+    [SerializeField]
+    private float SpawnEvery = 2f;
+
+    private void DoRandomSpawner()
+    {
+        var random = objectSpawners.OrderBy(_ => Guid.NewGuid()).First();
+        random.SpawnRandom();
+    }
+    void Start()
+    {
+        InvokeRepeating("DoRandomSpawner", 0f, SpawnEvery);
+
+    }
+}
+

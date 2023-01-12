f48a082dac36d2df0092734f5c637eab32e137b2
Retsav <ddadanski@edu.cdv.pl>
2023-01-12 21:35:52 +0100

    Wiecej wariantow budynkow, bugfixes

--- CatAway/Assets/Scripts/BuildingChecker.cs
+++ CatAway/Assets/Scripts/BuildingChecker.cs
@@ -8,7 +8,11 @@ public class BuildingChecker : MonoBehaviour
     [SerializeField] private V_SpawnerManager manager;
     private void OnTriggerExit2D(Collider2D collision)
     {
+        //To jest obrzydliwie napisane, ale nie mam ju¿ si³y.
         if (collision.gameObject.CompareTag("Platform"))
+        {
+            return;
+        } else if (collision.gameObject.CompareTag("Collectible"))
         {
             return;
         }

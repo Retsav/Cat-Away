81805c60d63cdaa1849d9e7913c97f5d71bca4e6
Retsav <ddadanski@edu.cdv.pl>
2023-01-17 23:06:36 +0100

    Fixed movement, added speed increasing curve

---
+++ CatAway/Assets/Scripts/SpeedManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedManager : MonoBehaviour
+{
+    [SerializeField] private Score score;
+    [SerializeField] private ObjectSpawner objectSpawner;
+    [SerializeField] private AnimationCurve speedCurve;
+    [SerializeField] private int maxPoints;
+  
+
+    void Start()
+    {
+        
+        
+    }
+
+    void Update()
+    {
+        var point = score.Points / maxPoints; 
+        var speedIntersection = speedCurve.Evaluate(point);
+        objectSpawner.DesiredSpeedOverride = speedIntersection;
+        Debug.Log(point);
+    }
+}
